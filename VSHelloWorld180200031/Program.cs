400ed7e30d1b522c4484a1ac7970541a5ae20b0d
Skarthis <32575517+Skarthis@users.noreply.github.com>
2018-10-08 16:59:06 +0100

    adicionado comentario

--- VSHelloWorld180200031/Program.cs
+++ VSHelloWorld180200031/Program.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Logging;
 
 namespace VSHelloWorld180200031
 {
+    //comentario de teste
     public class Program
     {
         public static void Main(string[] args)
